8f2b40a7d9660837a913857e96188a7dfe22eb29
kuroneko2539 <alex_daniels@fashionpocket.jp>
2018-12-03 15:12:26 +0900

    2016 day20

--- 2016/day20.py
+++ 2016/day20.py
@@ -13,5 +13,36 @@ for block in blockedList:
     blockedStarts.append(int(block.split("-")[0]))
     blockedEnds.append(int(block.split("-")[1]))
 
-blockedStarts = sorted(blockedStarts)
-blockedEnds = sorted(blockedEnds)
+sortedEnds = sorted(blockedEnds)
+sortedStarts = sorted(blockedStarts)
+
+maxSoFar = 0
+
+for i in range(len(sortedStarts)):
+    start = sortedStarts[i]
+    if start <= maxSoFar + 1:
+        index = blockedStarts.index(sortedStarts[i])
+        end = blockedEnds[index]
+        if end > maxSoFar:
+            maxSoFar = end
+    else:
+        print(maxSoFar + 1)
+        break
+
+numAllowed = 0
+
+for i in range(len(sortedStarts)):
+    start = sortedStarts[i]
+    if start <= maxSoFar + 1:
+        index = blockedStarts.index(sortedStarts[i])
+        end = blockedEnds[index]
+        if end > maxSoFar:
+            maxSoFar = end
+    else:
+        numAllowed += (start - (maxSoFar + 1))
+        index = blockedStarts.index(sortedStarts[i])
+        end = blockedEnds[index]
+        if end > maxSoFar:
+            maxSoFar = end
+    
+print(numAllowed)
